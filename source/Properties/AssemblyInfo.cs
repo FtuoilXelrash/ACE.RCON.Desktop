6d5810cc6c0b69e742a54c93467c24082c48ba08
Ftuoil_Xelrash <37750686+FtuoilXelrash@users.noreply.github.com>
2025-12-09 23:26:11 -0600

    Update AssemblyInfo.cs

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ACE RCON Desktop")]
 [assembly: AssemblyDescription("Desktop RCON client for ACE (Asheron's Call Emulator) servers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ftuoil Xelrash of Darktide")]
 [assembly: AssemblyProduct("ACE.RCON.Desktop")]
-[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyCopyright("Copyright © 2025 - Ftuoil Xelrash of Darktide")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.10.0")]
+[assembly: AssemblyFileVersion("0.0.10.0")]
